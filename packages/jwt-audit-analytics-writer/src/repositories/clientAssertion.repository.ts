148218f6593007b96a46b7fd97d1c593a785786b
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2025-02-26 22:13:14 +0100

    Refactor references

--- packages/jwt-audit-analytics-writer/src/repositories/clientAssertion.repository.ts
+++ packages/jwt-audit-analytics-writer/src/repositories/clientAssertion.repository.ts
@@ -5,12 +5,12 @@ import { config } from "../config/config.js";
 import { buildColumnSet } from "../utilities/pgHelper.js";
 import { GeneratedTokenAuditDetails } from "../model/domain/models.js";
 import {
-  GeneratedTokenMapping,
+  ClientAssertionMapping,
   JwtGeneratedDatabaseTable,
 } from "../model/db.js";
 
 export function clientAssertionRepository(t: ITask<unknown>) {
-  const generatedTokenTable = JwtGeneratedDatabaseTable.generated_token;
+  const clientAssertionTable = JwtGeneratedDatabaseTable.client_assertion;
 
   return {
     async insert(
@@ -18,36 +18,28 @@ export function clientAssertionRepository(t: ITask<unknown>) {
       records: GeneratedTokenAuditDetails[]
     ): Promise<void> {
       try {
-        const GeneratedTokenMapping: GeneratedTokenMapping = {
-          jwt_id: (record) => record.jwtId,
-          correlation_id: (record) => record.correlationId,
-          issued_at: (record) => new Date(record.issuedAt),
-          client_id: (record) => record.clientId,
-          organization_id: (record) => record.organizationId,
-          agreement_id: (record) => record.agreementId,
-          eservice_id: (record) => record.eserviceId,
-          descriptor_id: (record) => record.descriptorId,
-          purpose_id: (record) => record.purposeId,
-          purpose_version_id: (record) => record.purposeVersionId,
-          algorithm: (record) => record.algorithm,
-          key_id: (record) => record.keyId,
-          audience: (record) => record.audience,
-          subject: (record) => record.subject,
-          not_before: (record) => new Date(record.notBefore),
-          expiration_time: (record) => new Date(record.expirationTime),
-          issuer: (record) => record.issuer,
-          client_assertion_jwt_id: (record) => record.clientAssertion.jwtId,
+        const clientAssertionMapping: ClientAssertionMapping = {
+          jwt_id: (record) => record.clientAssertion.jwtId,
+          issued_at: (record) => new Date(record.clientAssertion.issuedAt),
+          algorithm: (record) => record.clientAssertion.algorithm,
+          key_id: (record) => record.clientAssertion.keyId,
+          issuer: (record) => record.clientAssertion.issuer,
+          subject: (record) => record.clientAssertion.subject,
+          audience: (record) => record.clientAssertion.audience,
+          expiration_time: (record) =>
+            new Date(record.clientAssertion.expirationTime),
         };
 
-        const tokenAuditTableName = `${generatedTokenTable}${config.mergeTableSuffix}`;
+        const clientAssertionTableName = `${clientAssertionTable}${config.mergeTableSuffix}`;
 
-        const tokenAuditColumnSet = buildColumnSet<GeneratedTokenAuditDetails>(
-          pgp,
-          GeneratedTokenMapping,
-          tokenAuditTableName
-        );
+        const clientAssertionColumnSet =
+          buildColumnSet<GeneratedTokenAuditDetails>(
+            pgp,
+            clientAssertionMapping,
+            clientAssertionTableName
+          );
 
-        await t.none(pgp.helpers.insert(records, tokenAuditColumnSet));
+        await t.none(pgp.helpers.insert(records, clientAssertionColumnSet));
       } catch (error: unknown) {
         throw genericInternalError(
           `Error inserting into client_assertion staging table: ${error}`
@@ -58,68 +50,38 @@ export function clientAssertionRepository(t: ITask<unknown>) {
     async merge(): Promise<void> {
       try {
         await t.none(`
-            MERGE INTO ${config.dbSchemaName}.${generatedTokenTable} AS target
-            USING ${config.dbSchemaName}.${generatedTokenTable}${config.mergeTableSuffix} AS source
-            ON target.jwt_id = source.jwt_id
-            WHEN MATCHED THEN 
+            MERGE INTO ${config.dbSchemaName}.${clientAssertionTable} AS target 
+            USING ${config.dbSchemaName}.${clientAssertionTable}${config.mergeTableSuffix} AS source
+              ON target.jwt_id = source.jwt_id
+            WHEN MATCHED THEN
               UPDATE
-                SET correlation_id       = source.correlation_id,
-                    issued_at            = source.issued_at,
-                    client_id            = source.client_id,
-                    organization_id      = source.organization_id,
-                    agreement_id         = source.agreement_id,
-                    eservice_id          = source.eservice_id,
-                    descriptor_id        = source.descriptor_id,
-                    purpose_id           = source.purpose_id,
-                    purpose_version_id   = source.purpose_version_id,
-                    algorithm            = source.algorithm,
-                    key_id               = source.key_id,
-                    audience             = source.audience,
-                    subject              = source.subject,
-                    not_before           = source.not_before,
-                    expiration_time      = source.expiration_time,
-                    issuer               = source.issuer,
-                    client_assertion_jwt_id = source.client_assertion_jwt_id
-            WHEN NOT MATCHED THEN 
+                SET issued_at       = source.issued_at,
+                    algorithm       = source.algorithm,
+                    key_id          = source.key_id,
+                    issuer          = source.issuer,
+                    subject         = source.subject,
+                    audience        = source.audience,
+                    expiration_time = source.expiration_time
+            WHEN NOT MATCHED THEN
               INSERT (
-                jwt_id,
-                correlation_id,
-                issued_at,
-                client_id,
-                organization_id,
-                agreement_id,
-                eservice_id,
-                descriptor_id,
-                purpose_id,
-                purpose_version_id,
-                algorithm,
-                key_id,
-                audience,
-                subject,
-                not_before,
-                expiration_time,
-                issuer,
-                client_assertion_jwt_id
-              )
+                jwt_id, 
+                issued_at, 
+                algorithm, 
+                key_id, 
+                issuer, 
+                subject, 
+                audience, 
+                expiration_time
+                )
               VALUES (
-                source.jwt_id,
-                source.correlation_id,
-                source.issued_at,
-                source.client_id,
-                source.organization_id,
-                source.agreement_id,
-                source.eservice_id,
-                source.descriptor_id,
-                source.purpose_id,
-                source.purpose_version_id,
-                source.algorithm,
-                source.key_id,
-                source.audience,
-                source.subject,
-                source.not_before,
-                source.expiration_time,
-                source.issuer,
-                source.client_assertion_jwt_id
+                source.jwt_id, 
+                source.issued_at, 
+                source.algorithm, 
+                source.key_id, 
+                source.issuer, 
+                source.subject, 
+                source.audience, 
+                source.expiration_time
               );
           `);
       } catch (error: unknown) {
@@ -132,7 +94,7 @@ export function clientAssertionRepository(t: ITask<unknown>) {
     async clean(): Promise<void> {
       try {
         await t.none(
-          `TRUNCATE TABLE ${config.dbSchemaName}.${generatedTokenTable}${config.mergeTableSuffix};`
+          `TRUNCATE TABLE ${config.dbSchemaName}.${clientAssertionTable}${config.mergeTableSuffix};`
         );
       } catch (error: unknown) {
         throw genericInternalError(
--- packages/jwt-audit-analytics-writer/src/repositories/generatedToken.repository.ts
+++ packages/jwt-audit-analytics-writer/src/repositories/generatedToken.repository.ts
@@ -5,12 +5,12 @@ import { config } from "../config/config.js";
 import { buildColumnSet } from "../utilities/pgHelper.js";
 import { GeneratedTokenAuditDetails } from "../model/domain/models.js";
 import {
-  ClientAssertionMapping,
+  GeneratedTokenMapping,
   JwtGeneratedDatabaseTable,
 } from "../model/db.js";
 
 export function generatedTokenRepository(t: ITask<unknown>) {
-  const clientAssertionTable = JwtGeneratedDatabaseTable.client_assertion;
+  const generatedTokenTable = JwtGeneratedDatabaseTable.generated_token;
 
   return {
     async insert(
@@ -18,31 +18,39 @@ export function generatedTokenRepository(t: ITask<unknown>) {
       records: GeneratedTokenAuditDetails[]
     ): Promise<void> {
       try {
-        const clientAssertionMapping: ClientAssertionMapping = {
-          jwt_id: (record) => record.clientAssertion.jwtId,
-          issued_at: (record) => new Date(record.clientAssertion.issuedAt),
-          algorithm: (record) => record.clientAssertion.algorithm,
-          key_id: (record) => record.clientAssertion.keyId,
-          issuer: (record) => record.clientAssertion.issuer,
-          subject: (record) => record.clientAssertion.subject,
-          audience: (record) => record.clientAssertion.audience,
-          expiration_time: (record) =>
-            new Date(record.clientAssertion.expirationTime),
+        const generatedTokenMapping: GeneratedTokenMapping = {
+          jwt_id: (record) => record.jwtId,
+          correlation_id: (record) => record.correlationId,
+          issued_at: (record) => new Date(record.issuedAt),
+          client_id: (record) => record.clientId,
+          organization_id: (record) => record.organizationId,
+          agreement_id: (record) => record.agreementId,
+          eservice_id: (record) => record.eserviceId,
+          descriptor_id: (record) => record.descriptorId,
+          purpose_id: (record) => record.purposeId,
+          purpose_version_id: (record) => record.purposeVersionId,
+          algorithm: (record) => record.algorithm,
+          key_id: (record) => record.keyId,
+          audience: (record) => record.audience,
+          subject: (record) => record.subject,
+          not_before: (record) => new Date(record.notBefore),
+          expiration_time: (record) => new Date(record.expirationTime),
+          issuer: (record) => record.issuer,
+          client_assertion_jwt_id: (record) => record.clientAssertion.jwtId,
         };
 
-        const clientAssertionTableName = `${clientAssertionTable}${config.mergeTableSuffix}`;
+        const tokenAuditTableName = `${generatedTokenTable}${config.mergeTableSuffix}`;
 
-        const clientAssertionColumnSet =
-          buildColumnSet<GeneratedTokenAuditDetails>(
-            pgp,
-            clientAssertionMapping,
-            clientAssertionTableName
-          );
+        const tokenAuditColumnSet = buildColumnSet<GeneratedTokenAuditDetails>(
+          pgp,
+          generatedTokenMapping,
+          tokenAuditTableName
+        );
 
-        await t.none(pgp.helpers.insert(records, clientAssertionColumnSet));
+        await t.none(pgp.helpers.insert(records, tokenAuditColumnSet));
       } catch (error: unknown) {
         throw genericInternalError(
-          `Error inserting into generated_token staging table: ${error}`
+          `Error inserting into generated_client staging table: ${error}`
         );
       }
     },
@@ -50,43 +58,73 @@ export function generatedTokenRepository(t: ITask<unknown>) {
     async merge(): Promise<void> {
       try {
         await t.none(`
-            MERGE INTO ${config.dbSchemaName}.${clientAssertionTable} AS target 
-            USING ${config.dbSchemaName}.${clientAssertionTable}${config.mergeTableSuffix} AS source
-              ON target.jwt_id = source.jwt_id
-            WHEN MATCHED THEN
+            MERGE INTO ${config.dbSchemaName}.${generatedTokenTable} AS target
+            USING ${config.dbSchemaName}.${generatedTokenTable}${config.mergeTableSuffix} AS source
+            ON target.jwt_id = source.jwt_id
+            WHEN MATCHED THEN 
               UPDATE
-                SET issued_at       = source.issued_at,
-                    algorithm       = source.algorithm,
-                    key_id          = source.key_id,
-                    issuer          = source.issuer,
-                    subject         = source.subject,
-                    audience        = source.audience,
-                    expiration_time = source.expiration_time
-            WHEN NOT MATCHED THEN
+                SET correlation_id       = source.correlation_id,
+                    issued_at            = source.issued_at,
+                    client_id            = source.client_id,
+                    organization_id      = source.organization_id,
+                    agreement_id         = source.agreement_id,
+                    eservice_id          = source.eservice_id,
+                    descriptor_id        = source.descriptor_id,
+                    purpose_id           = source.purpose_id,
+                    purpose_version_id   = source.purpose_version_id,
+                    algorithm            = source.algorithm,
+                    key_id               = source.key_id,
+                    audience             = source.audience,
+                    subject              = source.subject,
+                    not_before           = source.not_before,
+                    expiration_time      = source.expiration_time,
+                    issuer               = source.issuer,
+                    client_assertion_jwt_id = source.client_assertion_jwt_id
+            WHEN NOT MATCHED THEN 
               INSERT (
-                jwt_id, 
-                issued_at, 
-                algorithm, 
-                key_id, 
-                issuer, 
-                subject, 
-                audience, 
-                expiration_time
-                )
+                jwt_id,
+                correlation_id,
+                issued_at,
+                client_id,
+                organization_id,
+                agreement_id,
+                eservice_id,
+                descriptor_id,
+                purpose_id,
+                purpose_version_id,
+                algorithm,
+                key_id,
+                audience,
+                subject,
+                not_before,
+                expiration_time,
+                issuer,
+                client_assertion_jwt_id
+              )
               VALUES (
-                source.jwt_id, 
-                source.issued_at, 
-                source.algorithm, 
-                source.key_id, 
-                source.issuer, 
-                source.subject, 
-                source.audience, 
-                source.expiration_time
+                source.jwt_id,
+                source.correlation_id,
+                source.issued_at,
+                source.client_id,
+                source.organization_id,
+                source.agreement_id,
+                source.eservice_id,
+                source.descriptor_id,
+                source.purpose_id,
+                source.purpose_version_id,
+                source.algorithm,
+                source.key_id,
+                source.audience,
+                source.subject,
+                source.not_before,
+                source.expiration_time,
+                source.issuer,
+                source.client_assertion_jwt_id
               );
           `);
       } catch (error: unknown) {
         throw genericInternalError(
-          `Error merging staging to target generated_token table: ${error}`
+          `Error merging staging to target generated_client table: ${error}`
         );
       }
     },
@@ -94,17 +132,17 @@ export function generatedTokenRepository(t: ITask<unknown>) {
     async clean(): Promise<void> {
       try {
         await t.none(
-          `TRUNCATE TABLE ${config.dbSchemaName}.${clientAssertionTable}${config.mergeTableSuffix};`
+          `TRUNCATE TABLE ${config.dbSchemaName}.${generatedTokenTable}${config.mergeTableSuffix};`
         );
       } catch (error: unknown) {
         throw genericInternalError(
-          `Error cleaning staging generated_token table: ${error}`
+          `Error cleaning staging generated_client table: ${error}`
         );
       }
     },
   };
 }
 
-export type GeneratedTokenRepository = ReturnType<
+export type GenerateClientRepository = ReturnType<
   typeof generatedTokenRepository
 >;
