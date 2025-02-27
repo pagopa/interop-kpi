e91bd554dbbb2bfcbf248745e9e88a3dd0874aeb
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2025-02-27 09:43:07 +0100

    Update sql with TIMESTAMPTZ

--- docker/postgres/init-db.sql
+++ docker/postgres/init-db.sql
@@ -2,19 +2,19 @@ CREATE SCHEMA IF NOT EXISTS jwt_generated;
 
 CREATE TABLE IF NOT EXISTS jwt_generated.client_assertion_audit_details (
     jwt_id UUID PRIMARY KEY,
-    issued_at TIMESTAMP NOT NULL,
+    issued_at TIMESTAMPTZ NOT NULL,
     algorithm VARCHAR(50) NOT NULL,
     key_id VARCHAR(255) NOT NULL,
     issuer VARCHAR(255) NOT NULL,
     subject UUID NOT NULL,
     audience VARCHAR(255) NOT NULL,
-    expiration_time TIMESTAMP NOT NULL
+    expiration_time TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS jwt_generated.generated_token_audit_details (
     jwt_id UUID PRIMARY KEY,
     correlation_id UUID NOT NULL,
-    issued_at TIMESTAMP NOT NULL,
+    issued_at TIMESTAMPTZ NOT NULL,
     client_id UUID NOT NULL,
     organization_id UUID NOT NULL,
     agreement_id UUID NOT NULL,
@@ -26,8 +26,8 @@ CREATE TABLE IF NOT EXISTS jwt_generated.generated_token_audit_details (
     key_id VARCHAR(255) NOT NULL,
     audience VARCHAR(255) NOT NULL,
     subject VARCHAR(255) NOT NULL,
-    not_before TIMESTAMP NOT NULL,
-    expiration_time TIMESTAMP NOT NULL,
+    not_before TIMESTAMPTZ NOT NULL,
+    expiration_time TIMESTAMPTZ NOT NULL,
     issuer VARCHAR(255) NOT NULL,
     client_assertion_jwt_id UUID NOT NULL,
     CONSTRAINT fk_client_assertion FOREIGN KEY (client_assertion_jwt_id) REFERENCES jwt_generated.client_assertion_audit_details(jwt_id) ON DELETE CASCADE
