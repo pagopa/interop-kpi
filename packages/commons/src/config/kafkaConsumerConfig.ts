1a11bc6c63226a5899cfebc24e5ed389808e4bf1
diego-lon <diego.longo@digitouch.it>
2025-03-12 17:23:49 +0100

    kafka naming variables

--- packages/commons/src/config/kafkaConsumerConfig.ts
+++ packages/commons/src/config/kafkaConsumerConfig.ts
@@ -6,14 +6,14 @@ export const KafkaConsumerConfig = KafkaConfig.and(AWSConfig).and(
   z
     .object({
       KAFKA_GROUP_ID: z.string(),
-      TOPIC_STARTING_OFFSET: z
+      KAFKA_TOPIC_STARTING_OFFSET: z
         .union([z.literal("earliest"), z.literal("latest")])
         .default("latest"),
       RESET_CONSUMER_OFFSETS: z.string().default("false"),
     })
     .transform((c) => ({
       kafkaGroupId: c.KAFKA_GROUP_ID,
-      topicStartingOffset: c.TOPIC_STARTING_OFFSET,
+      topicStartingOffset: c.KAFKA_TOPIC_STARTING_OFFSET,
       resetConsumerOffsets: c.RESET_CONSUMER_OFFSETS.toLowerCase() === "true",
     }))
 );
@@ -21,17 +21,20 @@ export type KafkaConsumerConfig = z.infer<typeof KafkaConsumerConfig>;
 
 export const KafkaBatchConsumerConfig = z
   .object({
-    AVERAGE_KAFKA_MESSAGE_SIZE_IN_BYTES: z.coerce.number(),
-    MESSAGES_TO_READ_PER_BATCH: z.coerce.number(),
-    MAX_WAIT_KAFKA_BATCH_MILLIS: z.coerce.number(),
+    KAFKA_AVERAGE_MESSAGE_SIZE_IN_BYTES: z.coerce.number(),
+    KAFKA_MESSAGES_TO_READ_PER_BATCH: z.coerce.number(),
+    KAFKA_MAX_WAIT_KAFKA_BATCH_MILLIS: z.coerce.number(),
   })
   .transform((c) => {
     const minBytes =
-      c.AVERAGE_KAFKA_MESSAGE_SIZE_IN_BYTES * c.MESSAGES_TO_READ_PER_BATCH;
+      c.KAFKA_AVERAGE_MESSAGE_SIZE_IN_BYTES *
+      c.KAFKA_MESSAGES_TO_READ_PER_BATCH;
     return {
       minBytes,
-      maxWaitKafkaBatchMillis: c.MAX_WAIT_KAFKA_BATCH_MILLIS,
-      sessionTimeoutMillis: Math.round(c.MAX_WAIT_KAFKA_BATCH_MILLIS * 1.5),
+      maxWaitKafkaBatchMillis: c.KAFKA_MAX_WAIT_KAFKA_BATCH_MILLIS,
+      sessionTimeoutMillis: Math.round(
+        c.KAFKA_MAX_WAIT_KAFKA_BATCH_MILLIS * 1.5
+      ),
       maxBytes: Math.round(minBytes * 1.25),
     };
   });
