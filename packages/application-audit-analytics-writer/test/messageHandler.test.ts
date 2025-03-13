26c4ef0ef4a2ebe2b9434e3873a59e6928bf8c41
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2025-03-13 11:05:40 +0100

    Update tests

--- packages/application-audit-analytics-writer/test/messageHandler.test.ts
+++ packages/application-audit-analytics-writer/test/messageHandler.test.ts
@@ -1,18 +1,16 @@
 import { describe, expect, it } from "vitest";
 import { KafkaMessage } from "kafkajs";
-import { kafkaMissingMessagesValue } from "pagopa-interop-kpi-models";
 import { genericLogger } from "pagopa-interop-kpi-commons";
 import { handleMessages } from "../src/handlers/messagesHandler.js";
-import { config } from "../src/config/config.js";
 
 describe("Messages Handler tests", () => {
   describe("processMessage", () => {
-    it("missing kafka batch messages value should throw an error", async () => {
-      const invalidBatchMessages = undefined as unknown as KafkaMessage[];
+    it("processing empty messages from kafka should not throw error", async () => {
+      const emptyBatchMessages: KafkaMessage[] = [];
 
       await expect(
-        handleMessages(invalidBatchMessages, genericLogger)
-      ).rejects.toThrowError(kafkaMissingMessagesValue(config.kafkaTopic));
+        handleMessages(emptyBatchMessages, genericLogger)
+      ).resolves.toBeUndefined();
     });
   });
 });
