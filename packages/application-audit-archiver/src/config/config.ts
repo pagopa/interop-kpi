e4f07330715ce4844b04f832f1394b2559005ffd
diego-lon <diego.longo@digitouch.it>
2025-03-13 12:22:34 +0100

    fix env names

--- packages/application-audit-archiver/src/config/config.ts
+++ packages/application-audit-archiver/src/config/config.ts
@@ -4,22 +4,24 @@ import {
   KafkaTopicConfig,
   FileManagerConfig,
   KafkaBatchConsumerConfig,
+  AWSConfig,
 } from "pagopa-interop-kpi-commons";
 import { z } from "zod";
 
 const applicationAuditArchiverConfig = LoggerConfig.and(KafkaConsumerConfig)
   .and(KafkaTopicConfig)
+  .and(AWSConfig)
   .and(FileManagerConfig)
   .and(
     z
       .object({
         SERVICE_NAME: z.string(),
-        S3_BUCKET_NAME: z.string(),
-        GZ_COMPRESSION_LEVEL: z.number().default(6),
+        S3_BUCKET: z.string(),
+        GZ_COMPRESSION_LEVEL: z.coerce.number().default(6),
       })
       .transform((c) => ({
         serviceName: c.SERVICE_NAME,
-        s3BucketName: c.S3_BUCKET_NAME,
+        s3BucketName: c.S3_BUCKET,
         gzCompressionLevel: c.GZ_COMPRESSION_LEVEL,
       }))
   );
