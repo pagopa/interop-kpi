12bde85e743cbe1bf104d38271125824fb59a174
diego-lon <diego.longo@digitouch.it>
2025-03-14 16:30:30 +0100

    PIN-6372:  application-audit-archiver consumer implementation (#15)

--- packages/application-audit-archiver/src/handler/messagesHandler.ts
+++ packages/application-audit-archiver/src/handler/messagesHandler.ts
@@ -1,43 +1,51 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { KafkaMessage } from "kafkajs";
 import {
   FileManager,
   Logger,
   decodeKafkaMessage,
+  formatTimehhmmss,
 } from "pagopa-interop-kpi-commons";
 import {
   ApplicationAuditEvent,
-  kafkaMissingMessagesValue,
+  generateId,
+  genericInternalError,
 } from "pagopa-interop-kpi-models";
-import { KafkaMessage } from "kafkajs";
 import { config } from "../config/config.js";
+import { compressJson } from "../utilities/compression.js";
 
 export async function handleMessages(
   messages: KafkaMessage[],
   fileManager: FileManager,
   logger: Logger
 ) {
-  if (!messages) {
-    throw kafkaMissingMessagesValue(config.kafkaTopic);
-  }
-
   try {
-    for (const message of messages) {
-      const applicationAuditMessage = decodeKafkaMessage(
-        message,
-        ApplicationAuditEvent
-      );
+    const batch = messages.map((message) =>
+      decodeKafkaMessage(message, ApplicationAuditEvent)
+    );
 
-      const s3File = {
-        bucket: config.s3BucketName,
-        path: "TODO",
-        name: `${applicationAuditMessage.correlationId}.json`,
-        content: Buffer.from(JSON.stringify(applicationAuditMessage)),
-      };
+    const jsonString = JSON.stringify(batch);
+    const compressedBuffer = await compressJson(jsonString);
 
-      await fileManager.storeBytes(s3File, logger);
-    }
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const time = formatTimehhmmss(date);
+
+    const fileName = `${year}${month}${day}_${time}_${generateId()}.json.gz`;
+    const filePath = `year=${year}/month=${month}/day=${day}`;
+
+    const s3File = {
+      bucket: config.s3BucketName,
+      path: filePath,
+      name: fileName,
+      content: compressedBuffer,
+    };
+
+    await fileManager.storeBytes(s3File, logger);
   } catch (error) {
     const message = error instanceof Error ? error.message : "generic error";
-    throw Error(`Write operation failed - ${message}`);
+    throw genericInternalError(`Write operation failed - ${message}`);
   }
 }
