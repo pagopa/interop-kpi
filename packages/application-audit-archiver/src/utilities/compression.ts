66c79eca1f4e892e2bc28c01c56cb821daceb630
diego-lon <diego.longo@digitouch.it>
2025-03-14 14:24:29 +0100

    fix naming

--- packages/application-audit-archiver/src/utilities/compression.ts
+++ packages/application-audit-archiver/src/utilities/compression.ts
@@ -1,11 +1,17 @@
 /* eslint-disable functional/immutable-data */
+
 import { createGzip } from "zlib";
 import { pipeline } from "stream";
 import { Readable } from "stream";
 import { promisify } from "util";
 
 const pipelineAsync = promisify(pipeline);
-
+/**
+ * Compresses a JSON string using gzip.
+ *
+ * @param jsonString - The JSON string to be compressed.
+ * @returns A promise that resolves with a Buffer containing the gzipped data.
+ */
 export async function compressJson(jsonString: string): Promise<Buffer> {
   const gzipStream = createGzip();
   const readStream = Readable.from(jsonString);
