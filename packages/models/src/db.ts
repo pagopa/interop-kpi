172b4abb19a253c435ed6e0e156fcbad956ddd6b
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2025-03-14 16:39:37 +0100

    Fix linting

--- packages/models/src/db.ts
+++ packages/models/src/db.ts
@@ -12,7 +12,6 @@ export const ApplicationDbTable = {
 
 export type ApplicationDbTable =
   (typeof ApplicationDbTable)[keyof typeof ApplicationDbTable];
- 
 export const LoadBalancerLogTable = {
   logs: "alb_logs",
 } as const;
