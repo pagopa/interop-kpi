3a8f70df54f8f36859a93007dd6eb91ec76ce7c6
Eduardo Mihalache <eduardo.mihalache@digitouch.it>
2025-10-09 13:47:12 +0200

    refactor: removed unused dates functions

--- packages/commons/src/utils/date.ts
+++ packages/commons/src/utils/date.ts
@@ -1,17 +1,4 @@
 import { format } from "date-fns";
-import { formatInTimeZone } from "date-fns-tz";
-
-export function formatDateddMMyyyyHHmmss(date: Date): string {
-  return format(date, "dd/MM/yyyy HH:mm:ss");
-}
-
-export function formatDateyyyyMMddHHmmss(date: Date): string {
-  return format(date, "yyyyMMddHHmmss");
-}
-
-export function formatDateyyyyMMddThhmmss(date: Date): string {
-  return format(date, "yyyy-MM-dd'T'hh:mm:ss");
-}
 
 export function formatDateyyyyMMdd(date: Date): string {
   return format(date, "yyyyMMdd");
@@ -19,18 +6,6 @@ export function formatDateyyyyMMdd(date: Date): string {
 
 export function formatTimehhmmss(date: Date): string {
   return format(date, "hhmmss");
-}
-
-export function dateAtRomeZone(date: Date): string {
-  return formatInTimeZone(date, "Europe/Rome", "dd/MM/yyyy");
-}
-
-export function timeAtRomeZone(date: Date): string {
-  return formatInTimeZone(date, "Europe/Rome", "HH:mm:ss");
-}
-
-export function dateToSeconds(date: Date): number {
-  return Math.floor(date.getTime() / 1000);
 }
 
 export const dateToMilliseconds = (date: Date): number => {
