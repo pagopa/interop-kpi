4ca0655c30a2b3b26d6b7b31fe13f3722b0d9b9e
Eduardo Mihalache <eduardo.mihalache@digitouch.it>
2025-10-14 10:42:53 +0200

    fix(jwt-audit-analytics-writer): normalize microsecond timestamps to milliseconds for issued_at and issued_at_tz

--- packages/commons/src/utils/date.ts
+++ packages/commons/src/utils/date.ts
@@ -8,15 +8,19 @@ export function formatTimehhmmss(date: Date): string {
   return format(date, "hhmmss");
 }
 
-export const secondsToMilliseconds = (timestamp: number): number => {
-  if (timestamp.toString().length === 10) {
-    return timestamp * 1000;
+export const normalizeTimestampToMilliseconds = (timestamp: number): number => {
+  const integerPartOfTimestamp = Number(timestamp.toString().split(".")[0]);
+  const integerPartLength = integerPartOfTimestamp.toString().length;
+
+  if (integerPartLength === 10) {
+    // Seconds -> milliseconds
+    return integerPartOfTimestamp * 1000;
   }
 
-  return timestamp;
-};
+  if (integerPartLength > 13 && integerPartLength <= 16) {
+    // Microseconds -> milliseconds
+    return Math.floor(integerPartOfTimestamp / 1000);
+  }
 
-export const truncateTimestampDecimals = (value: number): number => {
-  const intPart = value.toString().split(".")[0];
-  return secondsToMilliseconds(Number(intPart));
+  return integerPartOfTimestamp;
 };
